64199d1cfaad04c23c0da53b58a7a852cec6c851
Raul Cristian Stroescu <raulstroescu12@gmail.com>
2025-03-08 22:19:57 +0200

    Flight Tracker v1.1 - CORS update

--- flight_backend/flight_backend/settings.py
+++ flight_backend/flight_backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'channels', 
+    'corsheaders',
     'api'
 ]
 
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'flight_backend.urls'
@@ -133,3 +135,7 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",
+]
